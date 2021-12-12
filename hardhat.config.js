070327c6d980da5bf6b27ffd5903f172e4a85af5
Javier Pozzi <javierpozzi@gmail.com>
2021-12-12 04:09:39 -0300

    Configure environment variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({path: "./.env"});
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +18,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_DEPLOY_ENDPOINT,
+      accounts: [process.env.RINKEBY_DEPLOY_PRIVATE_KEY],
+    },
+  },
 };
